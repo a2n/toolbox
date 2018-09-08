5ae658ebc5a3c310fa44c98bac1f0fdc5b91675c
盧利雄 <alan0098@gmail.com>
2018-09-08 14:21:56 +0800

    Passing gofmt

--- time.go
+++ time.go
@@ -8,38 +8,43 @@ import (
 	"github.com/beevik/ntp"
 )
 
+// TimeService The time service
 type TimeService struct {
 	tz   *time.Location
 	diff int64
 }
 
+// NewTimeService Creating
 func NewTimeService() *TimeService {
 	tz, e := time.LoadLocation("Asia/Taipei")
 	if e != nil {
 		log.Fatalf("%+v", e)
 	}
 	s := &TimeService{tz: tz}
+	s.sync()
 	go s.loop()
 	return s
 }
 
-func (this *TimeService) loop() {
-	this.sync()
-	for _ = range time.Tick(time.Minute) {
-		this.sync()
+// loop synchronization loop
+func (ts *TimeService) loop() {
+	for range time.Tick(time.Minute) {
+		ts.sync()
 	}
 }
 
-func (this *TimeService) sync() {
+// sync synchronization
+func (ts *TimeService) sync() {
 	t, e := ntp.Time("time.google.com")
 	if e != nil {
 		log.Printf("%+v", e)
 		return
 	}
-	atomic.StoreInt64(&this.diff, int64(t.Sub(time.Now())))
+	atomic.StoreInt64(&ts.diff, int64(t.Sub(time.Now())))
 }
 
-func (this *TimeService) Now() time.Time {
-	d := time.Duration(atomic.LoadInt64(&this.diff))
-	return time.Now().In(this.tz).Add(d)
+// Now Getting now time
+func (ts *TimeService) Now() time.Time {
+	d := time.Duration(atomic.LoadInt64(&ts.diff))
+	return time.Now().In(ts.tz).Add(d)
 }
